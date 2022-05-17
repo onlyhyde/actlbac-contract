b545b82842d97f52e24b884bfb03595005cec44b
kevin <onlyhyde@sogang.ac.kr>
2022-05-17 19:35:50 +0900

    feat : auto increment token ids

--- contracts/ArtNFT.sol
+++ contracts/ArtNFT.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ArtNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("ArtNFT", "ANT") {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 }
