4238f03985059b2613485033690a3e0e85a20d56
kevin <onlyhyde@sogang.ac.kr>
2022-05-18 14:29:29 +0900

    feat : original erc-721

--- contracts/ArtNFT.sol
+++ contracts/ArtNFT.sol
@@ -5,30 +5,23 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "./TokenIdAccessControl.sol";
 
-contract ArtNFT is ERC721, ERC721URIStorage, Ownable, TokenIdAccessControl {
+contract ArtNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-
-    bytes32 public constant VIEWER_ROLE = keccak256("VIEWER_ROLE");
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("ArtNFT", "ANT") {
-    }
+    constructor() ERC721("ArtNFT", "ANT") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-
         _setTokenURI(tokenId, uri);
-
-        _grantRole(tokenId, DEFAULT_ADMIN_ROLE, to);
-        _grantRole(tokenId, VIEWER_ROLE, to);
     }
 
     // The following functions are overrides required by Solidity.
+
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
@@ -37,29 +30,8 @@ contract ArtNFT is ERC721, ERC721URIStorage, Ownable, TokenIdAccessControl {
         public
         view
         override(ERC721, ERC721URIStorage)
-        onlyRole(tokenId, VIEWER_ROLE)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
-    }
-
-    function allowPermission(uint256 tokenId, address allowedAccount) 
-        public  
-        onlyRole(tokenId, DEFAULT_ADMIN_ROLE)
-    {
-        grantRole(tokenId, VIEWER_ROLE, allowedAccount);
-    }
-
-    function safeTransferFrom(
-      address from,
-      address to,
-      uint256 tokenId
-    ) public virtual override {
-      super.safeTransferFrom(from, to, tokenId);
-      
-      _grantRole(tokenId, DEFAULT_ADMIN_ROLE, to);
-      _revokeRole(tokenId, DEFAULT_ADMIN_ROLE, from);
-      _grantRole(tokenId, VIEWER_ROLE, to);
-      _revokeRole(tokenId, VIEWER_ROLE, from);
     }
 }
--- test/ArtNFT.test.js
+++ test/ArtNFT.test.js
@@ -68,45 +68,13 @@ describe('ArtNFT', function () {
     }
   });
 
-  it('tokenUri access reject test - others who has not permission should not be allowed', async function () {
+  it('tokenUri access reject test - others who has not permission should be allowed', async function () {
     let tokenId = 0;
     let tokenOwner = accounts[1];
     let other = accounts[2];
     const DUMMY_TOKEN_URI = "www.google.com";
     await this.artnft.safeMint(tokenOwner, DUMMY_TOKEN_URI, {from: owner});
-    expect(await this.artnft.tokenURI(tokenId, {from: other}).should.be.rejectedWith(EVM_REVERT));
-  });
-
-
-
-  it('token Owner can allow permission to others', async function () {
-    let tokenId = 0;
-    let nftowner = accounts[3];
-    let nftviewer = accounts[4];
-    const TOKENURI = "www.google.com";
-
-    let mint_result = await this.artnft.safeMint(nftowner, TOKENURI, {from: owner});
-    console.log(`one time minting gas Used : ${mint_result.receipt.gasUsed}`);
-
-    let nftowner_access_result = await this.artnft.tokenURI(tokenId, {from: nftowner});
-    console.log(`[nftowner] tokenURI : ${nftowner_access_result}`);
-    expect(nftowner_access_result).to.equal(TOKENURI);
-
-    // Allow Permission
-    let allow_reviewer_result = await this.artnft.allowPermission(tokenId, nftviewer, {from : nftowner});
-    console.log(`one time grant permission gas Used : ${allow_reviewer_result.receipt.gasUsed}`);
-    let nftviewer_access_result = await this.artnft.tokenURI(tokenId, {from: nftviewer});
-    console.log(`[nftviewer] tokenURI : ${nftviewer_access_result}`);
-    expect(nftviewer_access_result).to.equal(TOKENURI);
-  });
-
-  it('No permission No access', async function() {
-    let nftowner = accounts[4];
-    let nftviewer = accounts[5];
-    const TOKENURI = "www.google.com";
-
-    await this.artnft.safeMint(nftowner, TOKENURI, {from: owner});
-    expect(await this.artnft.allowPermission(0, nftviewer, {from : owner}).should.be.rejectedWith(EVM_REVERT));
+    expect(await this.artnft.tokenURI(tokenId, {from: other})).to.equal(DUMMY_TOKEN_URI);
   });
 
   it('token transfer', async function() {
@@ -120,14 +88,14 @@ describe('ArtNFT', function () {
     expect(await this.artnft.tokenURI(tokenId, {from: newOwner})).to.equal(DUMMY_TOKEN_URI);
   });
 
-  it('should not access token after token transfer', async function() {
+  it('can access token after token transfer', async function() {
     let tokenId = 0;
     let tokenOwner = accounts[1];
     let newOwner = accounts[2];
     const DUMMY_TOKEN_URI = "www.google.com";
     await this.artnft.safeMint(tokenOwner, DUMMY_TOKEN_URI, {from: owner});
     await this.artnft.safeTransferFrom(tokenOwner, newOwner, tokenId, {from: tokenOwner});
-    expect(await this.artnft.tokenURI(tokenId, {from: tokenOwner}).should.be.rejectedWith(EVM_REVERT));
+    expect(await this.artnft.tokenURI(tokenId, {from: tokenOwner})).to.equal(DUMMY_TOKEN_URI);
   });
 });
 
